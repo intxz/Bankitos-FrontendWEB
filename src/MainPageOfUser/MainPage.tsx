349ead7f7fa815383ba056ed061499d0eb16ab4c
intxz <angel.redondo@estudiantat.upc.edu>
2024-05-14 19:36:09 +0200

    nav

--- src/MainPageOfUser/MainPage.tsx
+++ src/MainPageOfUser/MainPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import './MainPage.css'
 
 function MainPage() {
   const [token, setToken] = useState<string>("");
@@ -16,9 +17,18 @@ function MainPage() {
   }, []);
 
   return (
-    <div>
-      <h1>{token}</h1>
-      <h1>{_id}</h1>
+    <div className="main-container"> 
+      <nav className="nav-user"> 
+        <a href="1" className="nav-link">1</a>
+        <a href="2" className="nav-link">2</a>
+        <a href="3" className="nav-link">3</a>
+        <a href="4" className="nav-link">4</a>
+        <a href="5" className="nav-link">5</a>
+      </nav>
+      <main className="content-container">
+        <h1>{token}</h1>
+        <h1>{_id}</h1>
+      </main>
     </div>
   );
 }
