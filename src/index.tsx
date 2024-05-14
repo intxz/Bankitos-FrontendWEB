8a252470a2171f576d1e103823eadc90b486a611
intxz <angel.redondo@estudiantat.upc.edu>
2024-05-14 17:04:25 +0200

    button tdah

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -12,25 +12,33 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
+
+
 function RoutesApp() {
+  const [isFollowCursorActive, setIsFollowCursorActive] = useState(false);
+
+  const toggleFollowCursor = () => {
+    setIsFollowCursorActive((prev) => !prev);
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/main_page" element={<MainPage />} />
-        <Route path="/xd" element={<XD />} />
-      </Routes>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/main_page" element={<MainPage />} />
+          <Route path="/xd" element={<XD />} />
+        </Routes>
+      </Router>
+      <button className="btn" onClick={toggleFollowCursor}>
+        {isFollowCursorActive ? "Deactivate Follow Cursor" : "Activate Follow Cursor"}
+      </button>
+      {isFollowCursorActive && <FollowCursorWindow />}
+    </React.StrictMode>
   );
 }
 
-root.render(
-  <React.StrictMode>
-    <FollowCursorWindow />
-    <RoutesApp />
-  </React.StrictMode>,
-);
-
+root.render(<RoutesApp />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
