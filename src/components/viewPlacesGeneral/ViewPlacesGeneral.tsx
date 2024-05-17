608b2eb560da13f4a4a18fa478d27016ac50fbd5
Titi679 <titicarles@gmail.com>
2024-05-17 16:55:44 +0200

    filtro añadido en place y users

--- src/components/viewPlacesGeneral/ViewPlacesGeneral.tsx
+++ src/components/viewPlacesGeneral/ViewPlacesGeneral.tsx
@@ -8,6 +8,7 @@ const apiUrl = "http://localhost:3000";
 
 function ViewPlacesGeneral({ _id, token }: { _id: string; token: string }) {
   const [places, setPlaces] = useState<Place[]>([]);
+  const [searchText, setSearchText] = useState<string>("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,19 +35,42 @@ function ViewPlacesGeneral({ _id, token }: { _id: string; token: string }) {
     navigate(`/place/${placeId}`);
   };
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredPlaces = places.filter(place =>
+    place.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <div className="containerViewPlacesGeneral">
+      <div className="searchContainer">
+      <div className="searchInput">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchText}
+          onChange={handleSearchChange}
+          className="searchInput"
+        />
+      </div>
+      </div>
       <div className="buttonContainerViewPlacesGeneral">
-        {/* Render buttons for each place */}
-        {places.map((place) => (
-          <button
-            key={place._id}
-            onClick={() => handlePlaceClick(place._id || "")}
-            className="buttonViewPlacesGeneral"
-          >
-            {place.title}
-          </button>
-        ))}
+        {filteredPlaces.length === 0 ? (
+          <p>No places with this name</p>
+        ) : (
+          // Render buttons for each filtered place
+          filteredPlaces.map((place) => (
+            <button
+              key={place._id}
+              onClick={() => handlePlaceClick(place._id || "")}
+              className="buttonViewPlacesGeneral"
+            >
+              {place.title}
+            </button>
+          ))
+        )}
       </div>
     </div>
   );
--- src/components/viewUsersGeneral/ViewUsersGeneral.tsx
+++ src/components/viewUsersGeneral/ViewUsersGeneral.tsx
@@ -8,6 +8,7 @@ const apiUrl = "http://localhost:3000";
 
 function ViewUsersGeneral({ _id, token }: { _id: string; token: string }) {
   const [users, setUsers] = useState<User[]>([]);
+  const [searchText, setSearchText] = useState<string>("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,9 +31,20 @@ function ViewUsersGeneral({ _id, token }: { _id: string; token: string }) {
   }, [_id, token]);
 
   const handleUsersClick = (userId: string) => {
-    // Redirect to the place details page when a place button is clicked
+    // Redirect to the user details page when a user button is clicked
     navigate(`/user/${userId}`);
   };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    `${user.first_name} ${user.middle_name ? user.middle_name + " " : ""} ${user.last_name}`
+      .toLowerCase()
+      .includes(searchText.toLowerCase())
+  );
+
   const renderStars = (rating: string) => {
     const ratingNumber = parseInt(rating);
     let stars = [];
@@ -44,23 +56,38 @@ function ViewUsersGeneral({ _id, token }: { _id: string; token: string }) {
 
   return (
     <div className="containerViewUsersGeneral">
+      <div className="searchContainer">
+      <div className="searchInput">
+        <input
+          type="text"
+          placeholder="Search users..."
+          value={searchText}
+          onChange={handleSearchChange}
+          className="searchInput"
+        />
+      </div>
+      </div>
       <div className="buttonContainerViewUsersGeneral">
-        {/* Render buttons for each place */}
-        {users.map((user) => (
-          <button
-            key={user._id}
-            onClick={() => handleUsersClick(user._id || "")}
-            className="buttonViewUsersGeneral"
-          >
-            {user.first_name + " "}{" "}
-            {user.middle_name ? user.middle_name + " " : ""}{" "}
-            {" " + user.last_name}
-            <br />
-            {user.description ? "Description: " + user.description : null}
-            <br />
-            {renderStars(user.user_rating ? user.user_rating + " " : "")}
-          </button>
-        ))}
+        {filteredUsers.length === 0 ? (
+          <p>No users with this name</p>
+        ) : (
+          // Render buttons for each user
+          filteredUsers.map((user) => (
+            <button
+              key={user._id}
+              onClick={() => handleUsersClick(user._id || "")}
+              className="buttonViewUsersGeneral"
+            >
+              {user.first_name + " "}{" "}
+              {user.middle_name ? user.middle_name + " " : ""}{" "}
+              {" " + user.last_name}
+              <br />
+              {user.description ? "Description: " + user.description : null}
+              <br />
+              {renderStars(user.user_rating ? user.user_rating + " " : "")}
+            </button>
+          ))
+        )}
       </div>
     </div>
   );
